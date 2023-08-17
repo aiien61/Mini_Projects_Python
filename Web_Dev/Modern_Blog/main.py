9519e7b0e3e54e24d06ae263aba4a30ea6a657fb
aiien61 <aiien61will@gmail.com>
2023-08-18 00:34:38 +0800

    allow sending form

--- Web_Dev/Modern_Blog/main.py
+++ Web_Dev/Modern_Blog/main.py
@@ -1,7 +1,20 @@
-import requests, datetime
-from flask import Flask, render_template
+import datetime
+import requests
+import smtplib
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# Replaced it with your gamil address
+GMAIL = "YOUR-GMAIL@gmail.com"
+# Generate your gmail app password, and replace it.
+GMAIL_PASSWORD = "YOUR-APP-PASSWORD"
+HOSTS = {
+    "gmail": "smtp.gmail.com",
+    "yahoo": "smtp.mail.yahoo.com",
+    "hotmail": "smtp.live.com",
+    "outlook": "smtp-mail.outlook.com"
+}
 
 posts = requests.get(url="https://api.npoint.io/63c37f8dcb409044f216").json()
 today = datetime.datetime.now()
@@ -24,8 +37,14 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        for k, v in request.form.items():
+            print(f"{k.title()}: {v}")
+        send_mail(**request.form)
+        msg="Successfully sent your message"
+        return show_info(data=request.form, msg=msg)
     return render_template("contact.html")
 
 
@@ -37,6 +56,23 @@ def get_post(post_id: int):
     for post in posts:
         if post["id"] == post_id:
             return render_template("post.html", post=post)
+
+
+@app.route("/info")
+def show_info(data: dict, msg: str):
+    return render_template("info.html", data=data, msg=msg)
+
+
+def send_mail(**kwargs):
+    email_message = "\n".join([f"{k.title()}: {v}" for k, v in kwargs.items()])
+    with smtplib.SMTP(HOSTS["gmail"]) as connection:
+        connection.starttls()
+        connection.login(user=GMAIL, password=GMAIL_PASSWORD)
+        connection.sendmail(
+            from_addr=GMAIL,
+            to_addrs=GMAIL,
+            msg=f"Subject:New Message From {kwargs['name']}\n\n{email_message}"
+        )
 
 
 if __name__ == "__main__":
