d7bf26f08a1e31f5630bb3a8303c0887d3d3edb1
aiien61 <aiien61will@gmail.com>
2024-11-02 02:01:35 +0800

    add form to blog website

---
+++ Web_Dev/Flask_Practice/html-forms/blog_venv/main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from typing import Dict
+import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data: Dict[str, str] = request.form
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+        print(name, email, phone, message)
+        # TODO: Add auto email notification
+        return render_template("contact.html", headline="Successfully sent your message!")
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
