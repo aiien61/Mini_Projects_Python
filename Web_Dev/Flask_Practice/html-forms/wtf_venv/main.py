6cf5d10f0ede48b43b7cd6d4b330c52ba49b7636
aiien61 <aiien61will@gmail.com>
2024-11-03 17:02:06 +0800

    use bootstrap-flask framework to auto render form without using html

--- Web_Dev/Flask_Practice/html-forms/wtf_venv/main.py
+++ Web_Dev/Flask_Practice/html-forms/wtf_venv/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 
 '''
 On Windows type:
@@ -14,25 +15,28 @@ pip3 install -r requirements.txt
 app = Flask(__name__)
 app.secret_key = "something secret"
 
+bootstrap = Bootstrap5(app)
+
 class MyForm(FlaskForm):
-    email = EmailField(label="Email", validators=[DataRequired()])
-    password = PasswordField(label="Password", validators=[DataRequired()])
-    submit = SubmitField(label="Login")
+    email = EmailField(label="Email", validators=[DataRequired(), Email()])
+    password = PasswordField(label="Password", validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField(label="Login", render_kw={"class": "btn btn-primary btn-lg"})
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     form = MyForm()
-    form.validate_on_submit()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if form.email.data == 'admin@email.com' and form.password.data == '12345678':
+                return render_template('success.html')
+            else:
+                return render_template('denied.html')
     return render_template('login.html', form=form)
-
-@app.route("/submit", methods=['POST', 'GET'])
-def submit():
-    pass
-
 
 if __name__ == '__main__':
     app.run(debug=True)
